12b42276a601b4d6ca0be2e88b381e525c195a02
Blum3 <mason.galilee@gmail.com>
2023-01-25 19:43:41 +0100

    Create App and Project Preview component, delete unused files

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,64 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import ProjectPreview from "@/components/ProjectPreview.vue";
+
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
+    <h1> Galilée Mason </h1>
+    <p id="small_title"> UI, UX, Games, and Web Design </p>
   </header>
 
-  <RouterView />
+
+  <project-preview/>
+  <project-preview/>
+  <project-preview/>
+
+
+  <footer>
+    <p><a href="https://www.freepik.com/free-photo/3d-abstract-wave-pattern-background_15611737.htm#query=black%20shape&position=24&from_view=keyword">Image by rawpixel.com</a> on Freepik</p>
+  </footer>
 </template>
 
+
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+
+*
+{
+  font-family: Verdana, Arial, serif;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+header
+{
+  height: 100vh;
+  background-image: url("images/wave-bg.png");
+  /* flex-box thing */
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-size: cover;
+  margin-bottom: 30px;
+  color: rgba(239,239,51,1);
 }
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+h1{
+  font-size: 300%;
+  margin-bottom: 0;
+  font-weight: normal;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
+#small_title{
+  font-size: 125%;
 }
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+footer{
+  background-color: rgba(239,239,51,1);
+  color: black;
+  padding: 20px;
 }
 
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
 
-nav a:first-of-type {
-  border: 0;
-}
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
---
+++ src/components/ProjectPreview.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <div class="separator"></div>
+    <div class="container">
+      <h1> Project Title </h1>
+      
+      <img src="../images/wave-bg.png">
+      
+      <p> description description description description description </p>
+      <p> description description description description description </p>
+      <p> description description description description description </p>
+      <p> description description description description description </p>
+      <p> description description description description description </p>
+      <p> description description description description description </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProjectPreview"
+}
+</script>
+
+<style scoped>
+
+*{
+  color: rgba(239,239,51,1);
+  text-align: center;
+}
+
+h1{
+  padding: 20px
+}
+
+.container{
+  background-color: black;
+  padding-bottom: 40px;
+  padding-top: 30px;
+}
+
+.separator{
+  background-color: rgba(239,239,51,1);
+  width: 100%;
+  height: 20px;
+}
+
+img{
+  width: 60%;
+  height: 60%;
+  padding : 30px;
+}
+</style>
