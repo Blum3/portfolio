6d41bf1c99ce4a207dfc8f68164d4b186d104eff
Galilée Mason <mason.galilee@gmail.com>
2023-01-29 13:12:41 +0100

    Add scroll button

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,25 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import ProjectPreview from "@/components/ProjectPreview.vue";
+import jQuery from "jquery";
+const $ = jQuery;
+window.$ = $;
 
 </script>
 
+
+
 <template>
   <header>
-    <h1> Galilée Mason </h1>
+    <h1> Galilee Mason </h1>
     <p id="small_title"> UI, UX, Games, and Web Design </p>
+
+    <img @click="scroll" id="down" src="./images/arrow-down.png">
   </header>
 
-
+  <section id="section1">
+    <project-preview/>
+  </section>
   <project-preview/>
   <project-preview/>
   <project-preview/>
@@ -22,7 +31,32 @@ import ProjectPreview from "@/components/ProjectPreview.vue";
 </template>
 
 
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+    };
+  },
+  methods: {
+    scroll: function () {
+      document.getElementById('section1').scrollIntoView({
+        behavior: "smooth"
+      });
+    },
+  },
+};
+</script>
+
 <style scoped>
+html {
+  scroll-behavior: smooth;
+}
+
+@font-face {
+  font-family: TitleFont;
+  src: url(fonts/rogre.otf);
+}
 
 *
 {
@@ -44,13 +78,29 @@ header
 }
 
 h1{
-  font-size: 300%;
+  font-size: 380%;
   margin-bottom: 0;
   font-weight: normal;
+  text-decoration: none;
 }
 
 #small_title{
-  font-size: 125%;
+  font-size: 150%;
+}
+
+#down
+{
+  position: absolute;
+  top:85vh;
+  padding: 20px;
+  font-size: 2.5em;
+  width: 7%;
+  color: rgba(239,239,51,1);
+}
+#down:hover
+{
+  cursor: pointer;
+  width: 10%;
 }
 
 footer{
