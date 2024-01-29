f9c96c8ca5cce65508cb59157ac0454c96e4f0d3
Galilée MASON <mason.galilee@gmail.com>
2024-01-29 12:44:49 +0100

    Update "about me"

--- src/components/AboutMePage.vue
+++ src/components/AboutMePage.vue
@@ -47,7 +47,7 @@
   <div class="aboutme">
     <img src="/images/me2.png" class="picofme">
     <p>
-      My name is Galilée Mason, I am a 22 years old student at Paul Sabatier University and at the National School of Civic Aviation in M2 Human Machine Interaction. I am looking for an internship in UI/UX from 01/03/2024 to 01/08/2024.
+      My name is Galilée Mason, I am a 22 years old student at Paul Sabatier University and at the National School of Civic Aviation in M2 Human Machine Interaction. I am looking for an internship in UI/UX, web, or really any kind of back or front end development. My internship must take place for from 01/03/2024 to 01/08/2024 (4 to 6 months).
     </p>
     <p>
       Passionate about video games and design for a long time, I have the chance today to get involved in personal and collective projects around my passions, either at the university or during my free time. I am interested in the UI/UX aspect of computer systems. The purely visual aspect of my projects is important to me. I just came back from two semesters in Germany where I studied computer science at the HTW Berlin.
